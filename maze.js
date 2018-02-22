0a5fa0f19242cc79e2eb6702b093277331b829fa
JonathanJustavino <s3190208@mail.zih.tu-dresden.de>
2018-02-22 19:55:28 +0100

    shit commit

--- maze.js
+++ maze.js
@@ -42,10 +42,6 @@ Maze.prototype = {
             }
         }
     },
-
-    randomize: function (params) {
-
-    }
 }
 
 const TOP = 1
@@ -57,6 +53,12 @@ function MazeData(size) {
     this.grid = new Array(size * size)
     this.grid.fill(BOTH)
     this.size = size
+    this.directions = {
+        0: -1,
+        1: 1,
+        2: -this.size,
+        3: this.size,
+    }
 }
 
 MazeData.prototype = {
@@ -110,19 +112,106 @@ MazeData.prototype = {
                 s+=this.getBlock(x,y)
             }
         }
-        console.log(s);
+        // console.log(s);
+    },
+
+    path: function () {
+        for (let i = 0; i < this.size; i++) {
+            this.grid
+        }
+    },
+
+    rand: function (previous, pos) {
+        let left = pos - 1
+        let posRight = pos + 1
+        let up = pos - this.row
+        let down = pos + this.row
+
     }
 }
 
 
-let size = 30
+let size = 20
 let height = size*size
 let position = {
-    x: 25,
-    y: 25
+    x: 50,
+    y: 50
 }
 
 
-let maze = new Maze(canvas, height, position, size)
+let maze = new Maze(canvas, height, position, 10)
 
 maze.render()
+
+
+/**
+ *
+ * Directions to choose in a maze
+ * X  - Actual Position
+ * S  - Size of a row of the maze
+ * CP - Current Position
+ * P  - Next Position
+ *
+ * +-------++-------++-------++
+ * |       || X - S ||       ||
+ * +-------++-------++-------++
+ * | X - 1 ||   X   || X + 1 ||
+ * +-------++-------++-------++
+ * |       || X + S ||       ||
+ * +-------++-------++-------++
+ *
+ * Function: chooseDirection()
+ * conclude from chosen direction
+ * which wall to delete
+ * X < P
+ *  | X + 1 == P -> delete P's left wall
+ *  | -> delete P's top wall
+ * X > P
+ *  | X - 1 == P -> delete X's left wall
+ *  | -> delete X's top wall
+ *
+ * End when at Position maze[size of maze]
+ */
+
+
+
+function ran(maze) {
+    let size = maze.length
+    let sizeOfRow = Math.sqrt(size)
+    let path = []
+    let pos = 0
+    for (let i = 0; i < size; i++) {
+        path.push(pos)
+        // console.log(path);
+        let next = chooseDirection(sizeOfRow, pos)
+        // console.log(next);
+
+        pos = next
+    }
+}
+
+
+
+ran(maze.maze.grid)
+
+
+function chooseDirection(sizeOfRow, pos) {
+    let posLeft = pos - 1
+    let posRight = pos + 1
+    let up = pos - sizeOfRow
+    let down = pos + sizeOfRow
+    let DIR = {
+        0: posRight,
+        1: posLeft,
+        2: up,
+        3: down,
+    }
+    if (pos == 0 || up < 0) {
+        if(posLeft < 0){
+            return posRight
+        }
+        return DIR[Math.floor(Math.random() * 10) % 2]
+    }
+
+    return DIR[Math.floor(Math.random() * 10) % 4]
+}
